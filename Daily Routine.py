53ff3629cc1330f772cbc680578e23551f6ce167
abhirammallampati-cyber <abhiram.mallampati@gmail.com>
2026-07-04 15:03:26 -0500

    Add files via upload
    This is my daily routine on a normal day.

---
+++ Daily Routine.py
@@ -0,0 +1,16 @@
+print("My Daily Routine")
+
+# Step 1: Waking up
+print("I wake up at 7:30 AM.")
+
+# Step 2: Going to school
+print("I go to school at 9 AM.")
+
+# Step 3: Playing after school
+print("I play basketball with my friends in the evening.")
+
+# Step 4: Studying
+print("I study for 2 hours at night.")
+
+# total study hours in a week
+print("Total study hours in a week:", 2 * 7)
