5ba42d084a0968feb563b8bdc81f1727d50c933b
abhirammallampati-cyber <abhiram.mallampati@gmail.com>
2026-07-05 20:38:21 -0500

    Add files via upload

---
+++ Print.py
@@ -0,0 +1,14 @@
+#print hello sentence
+print("Hi, My name is Abhiram")
+
+#print number
+print(6)
+
+#print with new line
+print("hello \n")
+
+#print with multiple arguments
+print("Hello",5)
+
+#print with end
+print("Goodbye", end="@")
