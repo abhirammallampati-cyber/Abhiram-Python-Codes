1602d359418b52f4a1a53ba38128a0704c98eb0c
abhirammallampati-cyber <abhiram.mallampati@gmail.com>
2026-07-05 20:38:44 -0500

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,18 @@
+x=6
+y="Abhiram"
+a=7
+z="Hello"
+#print variables
+print("The value of x is", x)
+print(y)
+print(a)
+print(z)
+
+age=input("Enter your age")
+print("\nYour age is:",age)
+
+name=input("Enter your name")
+print("\nMy name is:", name)
+
+hobby=input("Enter your favorite hobby")
+print("\nMy favorite hobby is:", hobby)
