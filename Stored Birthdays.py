263b4d472e684e13ce6afabeae2169dbdfe3a9e6
abhirammallampati-cyber <abhiram.mallampati@gmail.com>
2026-07-08 18:16:41 -0500

    Add files via upload

---
+++ Stored Birthdays.py
@@ -0,0 +1,13 @@
+# make variables and assign dates to mine and friend's birthdays
+person1 = "Abhiram: July 30"
+person2 = "Pranav: February 22"
+person3 = "Rohan: July 10"
+person4 = "Nikhil: December 12"
+person5 = "Amit: June 19"
+
+# Print all variables
+print(person1)
+print(person2)
+print(person3)
+print(person4)
+print(person5)
