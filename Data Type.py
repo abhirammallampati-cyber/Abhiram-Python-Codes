5a6886d80064fa818d69cf821fc2678b843b77a3
abhirammallampati-cyber <abhiram.mallampati@gmail.com>
2026-07-08 20:41:16 -0500

    Add files via upload

---
+++ Data Type.py
@@ -0,0 +1,10 @@
+#check all the data types
+
+a=8
+print("the type of a:",type(a))
+b=3.7
+print("the types of b:", type(b))
+c="coding"
+print("the type of c:", type(c))
+d=True
+print("the type of d:",type(d))
