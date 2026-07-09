02ab3a713f4a49152e7c0a95bc38f2054280a67b
abhirammallampati-cyber <abhiram.mallampati@gmail.com>
2026-07-08 20:40:30 -0500

    Add files via upload

---
+++ DataTypeCasting.py
@@ -0,0 +1,25 @@
+name="Abhiram"
+age=13
+is_student=True
+weight=12.3
+
+print("Name:", name)
+print('Data Type of name is', type(name))
+
+print("Age:", age)
+print("Data Type of age is:", type(age))
+
+print("is_student:", is_student)
+print("Data Type of is_student is:", type(is_student))
+
+print("Weight:", weight)
+print("Data type of weight is:", type(weight))
+
+print("\n After Type Casting.....")
+age=str(age)
+print(age)
+print("Data Type of age is:", type(age))
+print("\n After Type Casting.....")
+weight=int(weight)
+print(weight)
+print("Data type of weight is:", type(weight))
