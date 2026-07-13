db5eb7b1aaf0d945f1385c42c2644aac8579e6d9
Abhiram_Python <abhiram.mallampati@gmail.com>
2026-07-12 20:32:27 -0500

    Add files via upload

---
+++ Comparison.py
@@ -0,0 +1,7 @@
+number=int(input("Enter your Number:"))
+if number > 15:
+    print("Your Number is greater than 15")
+if number == 15:
+    print("Your Number is equal to 15")
+else:
+    print("Your Number is less than 15")
