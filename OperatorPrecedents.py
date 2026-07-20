aae9d02c973cdc8a16ca5c1f454f75cf6a4a9603
Abhiram_Python <abhiram.mallampati@gmail.com>
2026-07-19 20:38:49 -0500

    Add files via upload

---
+++ OperatorPrecedents.py
@@ -0,0 +1,38 @@
+v = 20
+w = 30 
+x = 40 
+y = 50
+z = (v+w)* x/y
+print("The value of z is:", z)
+
+name = "Abhiram"
+age = 13
+if name == "Abhiram":
+   print("Welcome")
+else:
+   print("Goodbye")
+
+if name is "Abhiram" and age < 10:
+   print("Hello World!")
+else:
+   print("Have a Good Day!")
+
+num = int(input("Enter Your Numerator:"))
+numd = int(input("Enter Your Denominator:"))
+
+if num%numd==0:
+   print("This is a divisible number")
+else:
+   print("This is not a divisible number")
+
+mean1 = 38
+wrong_number = 36
+right_number = 56
+total = 40
+sum = mean1*total 
+print("The sum of 40 number:", sum)
+num2 = sum - ((wrong_number)-(right_number))
+print("sum - ((wrong_number)-(right_number)):", num2)
+
+mean2 = num2/total
+print(mean2)
